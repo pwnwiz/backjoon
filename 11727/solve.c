07b3c07294ae4302459c7d0ac3de77c324168f46
pwnwiz <pwnwiz@gmail.com>
2020-01-03 18:43:56 +0900

    add solve for 11727 in C++ langs
    Reviewed by Wizley.
    2020.01.03.
    
    I added solve for 11727 in C++ languages.
    
    I used DP to solve this prob.
    
    DP[i] = DP[i-1] + DP[i-2] + DP[i-2]

---
+++ 11727/solve.c
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/11727
+// 2×n 타일링 2
+// Written in C++ langs
+// 2020. 01. 03.
+// Wizley
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int DP[1000001]={0,};
+
+using namespace std;
+
+int main(){
+    int input;
+    scanf("%d", &input);
+    
+    DP[0] = 1;
+    DP[1] = 1;
+    for(int i=2; i<=input; i++){
+        DP[i] = (DP[i-1] + DP[i-2] + DP[i-2]) % 10007;
+    }
+    printf("%d\n", DP[input]);
+    
+}
+
