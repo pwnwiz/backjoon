7ad4fa2d720513b1616a8f441b08cd25a9311e6c
wizleysw <wizley@kakao.com>
2020-01-07 18:32:20 +0900

    add solve for 11722 in C++ langs
    Reviewed by Wizley.
    2020.01.07.
    
    I have added solve for 11722 in C++ languages.
    
    It was the same but only difference in +/- with 11053.

---
+++ 11722/solve.cpp
@@ -0,0 +1,36 @@
+// https://www.acmicpc.net/problem/11722
+// 가장 긴 감소하는 부분 수열
+// Written in C++ langs
+// 2020. 01. 07.
+// Wizley
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int DP[1001]={0,};
+int SEQUENCE[1001]={0,};
+
+int main() {
+    int size;
+    scanf("%d", &size);
+    
+    for(int i=1; i<=size; i++){
+        scanf("%d", &SEQUENCE[i]);
+    }
+    
+    fill_n(DP, 1001, 1);
+    
+    for(int i=2; i<=size; i++){
+        for(int j=1; j<i; j++){
+            if(SEQUENCE[i]<SEQUENCE[j]){
+                DP[i] = max(DP[i], DP[j]+1);
+            }
+        }
+    }
+    printf("%d\n", *max_element(begin(DP), DP+size+1));
+    return 0;
+}
+
