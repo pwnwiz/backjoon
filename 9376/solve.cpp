30195da5134ddeb1d6da7d0cf61076ddc360ba31
wizleysw <wizley@kakao.com>
2021-01-04 22:44:33 +0900

    fix logic bug on 9376
    Reviewed by Wizley.
    2021.01.04.
    
    There was logic bug on 9376 when sum up the result. if not check sum < 0 wrong result!

--- 9376/solve.cpp
+++ 9376/solve.cpp
@@ -1,7 +1,7 @@
 // https://www.acmicpc.net/problem/9376
 // 탈옥
 // Written in C++ langs
-// 2020. 10. 09.
+// 2021. 01. 04.
 // Wizley
 
 #include <iostream>
@@ -25,19 +25,24 @@ int dy[] = {-1,1,0,0};
 
 int getResult(){
     int result = 987654321;
-    
+
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
-            
+
             if(MAP[i][j] == '*') continue;
-            
+
             int sum = 0;
             for(int pr=0; pr<3; pr++){
                 sum += DOOR[i][j][pr];
             }
-            
+
             // If three prisoners open the same door
-            if(MAP[i][j] == '#') sum -= 2;
+            if(MAP[i][j] == '#'){
+                sum -= 2;
+            }
+
+            // FIXED ERROR : OOB
+            if(sum < 0) continue;
             
             result = min(result, sum);
         }
@@ -46,28 +51,28 @@ int getResult(){
 }
 
 void bfs(){
-    
+
     for(int pr=0; pr<prisoner.size(); pr++){
         int x = prisoner[pr].first;
         int y = prisoner[pr].second;
-        
+
         queue<pair<int,int>> que;
         que.push({x,y});
         DOOR[x][y][pr] = 0;
-        
+
         while(!que.empty()){
             int cx = que.front().first;
             int cy = que.front().second;
             que.pop();
-            
+
             for(int i=0; i<4; i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                
+
                 if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
-                
+
                 if(MAP[nx][ny] == '*') continue;
-                
+
                 if(MAP[nx][ny] == '#'){
                     if(DOOR[nx][ny][pr] == -1 || DOOR[nx][ny][pr]>DOOR[cx][cy][pr]+1){
                         DOOR[nx][ny][pr] = DOOR[cx][cy][pr]+1;
@@ -88,18 +93,18 @@ int main(){
     ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
     cin >> T;
-    
+
     for(int i=0; i<T; i++){
         cin >> N >> M;
-        
+
         N += 2;
         M += 2;
-        
+
         prisoner.clear();
         memset(DOOR, -1, sizeof(DOOR));
-        
+
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
                 if(!i || !j || i==N-1 || j==M-1){
@@ -107,16 +112,16 @@ int main(){
                     continue;
                 }
                 cin >> MAP[i][j];
-                
+
                 if(MAP[i][j] == '$'){
                     prisoner.push_back(make_pair(i,j));
                 }
             }
         }
-        
+
         // outdoor -> indoor
         prisoner.push_back({0,0});
-        
+
         bfs();
         cout << getResult() << "\n";
     }
